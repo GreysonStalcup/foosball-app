ce49088153490b6cc330b6fa5ff1d996cb1c17da
Mark Rodgers <mark@marknrodgers.com>
2023-03-09 22:58:00 -0800

    replace login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import "./App.css";
 import React, { useState, useEffect } from "react";
 import { Route, Routes, Navigate } from "react-router-dom";
 import Home from "./Components/Home";
 // import { listenForMatchData, saveMatchData } from "./firebase";
 import ManageTeams from "./Components/ManageTeams";
-import SignIn from "./Components/SignIn";
+import Login from "./pages/Login";
 import SignOut from "./auth/SignOut";
+
 function App() {
   return (
     <div id="app">
@@ -13,10 +13,11 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/manage-teams" element={<ManageTeams />} />
         <Route path="*" element={<Navigate to="/" />} />
-        <Route path="/login" element={<SignIn />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/auth/logout" element={<SignOut />} />
       </Routes>
     </div>
+
     // <div className="App">
     //   <h1>Foosball Tournament</h1>
     //   <MatchList matchData={matchData} onSaveMatchData={handleSaveMatchData} />
